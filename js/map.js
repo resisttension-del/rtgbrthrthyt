fdb6d2c327cd0dc31508dba89b948ef2460b1fd9
resisttension-del <resisttension@gmail.com>
2025-09-05 23:42:38 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -134,8 +134,8 @@ export async function createCrocodilosConstruction(scene, physicsController) {
     scene.add(sunLight, sunLight.target);
 
     // URL of the GLB model for the map
-    const GLB_MODEL_URL = 'https://raw.githubusercontent.com/thearthd/3d-models/main/Map/voidffa_temp_map.glb';
-
+  //  const GLB_MODEL_URL = 'https://raw.githubusercontent.com/thearthd/3d-models/main/Map/voidffa_temp_map.glb';
+const GLB_MODEL_URL = 'https://raw.githubusercontent.com/thearthd/3d-models/main/Map/croccodilosconstruction.glb';
     // 1) Load the GLB model into the scene and process it for collision detection
     let gltfGroup = null;
     let onGLBProgress = () => {};
