12c4c91a2a689d315b94051147bfb552b1539ba9
resisttension-del <resisttension@gmail.com>
2025-09-23 03:31:54 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -99,14 +99,7 @@ export async function createCrocodilosConstruction(targetSceneRoot, physicsContr
 
   const SCALE = 5;
     const rawSpawnPoints = [
-        new THREE.Vector3(-14, 7, -36), // 1
-        new THREE.Vector3(-2, 2, 37), // 2
-        new THREE.Vector3(0, 2, 0), // 3
-        new THREE.Vector3(2, 7, 34), // 4
-        new THREE.Vector3(-5, 2, -38), // 5
-        new THREE.Vector3(-18, 2, 12), // 6
-        new THREE.Vector3(11, 2, 23), // 7
-        new THREE.Vector3(-7, 7, -1), // 8
+        new THREE.Vector3(0, 90, 0), // h
     ];
   const spawnPoints = rawSpawnPoints.map(p => {
     const scaled = p.clone().multiplyScalar(SCALE / 5);
